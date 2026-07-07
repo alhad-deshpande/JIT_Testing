03506482ad03eb51b861eef8dfc18e5282857405
Ipshita-2205 <ipshita.das@ibm.com>
2026-07-07 03:43:33 -0400

    Added HelloWorld in test suite

---
+++ testcases/000_HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nHelloWorld");
+    }
+}
+
+
+/*
+    class Test
+    {
+	public int a;
+    }
+
+	Test obj = null;
+	//int a = obj.a;
+*/
