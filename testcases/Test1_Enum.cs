2a954d612019804b23e9a625240283878971374e
vikasgupta8 <vikas.gupta8@ibm.com>
2026-08-05 10:58:55 -0400

    Added Enum test

---
+++ testcases/Test1_Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+	public enum Level
+	{
+		Low,      // 0
+		Medium,   // 1
+		High      // 2
+	}
+
+	public static Level ppc64leHelloWorld()
+	{
+		return Level.Medium;
+	}
+
+	public static int Main()
+	{
+		Level result  = ppc64leHelloWorld();
+		Console.WriteLine(result);
+		return (int)result == 1 ? 0 : 1;
+	}
+}
